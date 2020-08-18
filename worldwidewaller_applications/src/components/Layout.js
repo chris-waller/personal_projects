a28f7f4d96e63aca69434cd417e3e2ba77678ae2
chris-waller <cwaller2@gmail.com>
2020-08-18 19:05:22 -0400

    Changed default theme

--- worldwidewaller_applications/src/components/Layout.js
+++ worldwidewaller_applications/src/components/Layout.js
@@ -21,14 +21,14 @@ import changeTheme from '../utilities/change-theme.js';
 export default class Layout extends Component {
 
   THEMES = [
+    { value: styles.default_theme, label: 'Default', className: styles.menuItem },
     { value: styles.forest_theme, label: 'Forest', className: styles.menuItem },
-    { value: styles.greyscale_theme, label: 'Greyscale', className: styles.menuItem },
-    { value: styles.default_theme, label: 'Ocean', className: styles.menuItem },    
+    { value: styles.greyscale_theme, label: 'Greyscale', className: styles.menuItem },    
     { value: styles.pilgrim_theme, label: 'Pilgrim', className: styles.menuItem },
   ];
   
   // set the default site theme here
-  DEFAULT_THEME = this.THEMES.find(theme => theme.value === styles.greyscale_theme);
+  DEFAULT_THEME = this.THEMES.find(theme => theme.value === styles.default_theme);
   
   /**
    * Constructor.
