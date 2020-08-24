4e40c400a25218caddff9dbf058dc2c6e637fb53
chris-waller <cwaller2@gmail.com>
2020-08-24 15:06:50 -0400

    Added education sections

--- worldwidewaller_applications/src/pages/page_components/resume/Education.js
+++ worldwidewaller_applications/src/pages/page_components/resume/Education.js
@@ -6,6 +6,9 @@ import classNames from 'classnames';
 // custom components
 import Collapsible from 'react-collapsible';
 
+// style imports
+import styles from './styles/education.scss';
+
 const Education = ({
   sectionClassName, triggerClassName, openClassName, contentClassName,
 }) => (
@@ -18,7 +21,26 @@ const Education = ({
     contentInnerClassName={contentClassName}
     open
   >
-    <p>My Education</p>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div>
+          <a
+            target="__blank"
+            href="https://academics.sheridancollege.ca/programs/computer-systems-technology-software-development-and-network-engineering"
+          >
+            Software Development and Network Engineering Advanced Diploma,
+            <span className={styles.honours}>
+              &nbsp;with Honours
+            </span>
+          </a>
+        </div>
+        <div>
+          <a target="__blank" href="https://www.sheridancollege.ca/">
+            Sheridan Institute of Technology and Advanced Learning, 2010
+          </a>
+        </div>
+      </div>
+    </div>
   </Collapsible>
 
 );
