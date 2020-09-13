b46a367dceb301b7db91fe6dba12ddf046051a89
chris-waller <cwaller2@gmail.com>
2020-09-13 18:01:11 -0400

    moved current web project into its own client folder for better organization

--- worldwidewaller_applications/client/build-utils/common-paths.js
+++ worldwidewaller_applications/client/build-utils/common-paths.js
@@ -6,4 +6,5 @@ module.exports = {
   projectRoot: PROJECT_ROOT,
   outputPath: path.join(PROJECT_ROOT, 'dist'),
   appEntry: path.join(PROJECT_ROOT, 'src'),
+  serverEntry: path.join(PROJECT_ROOT, 'server'),
 };
