e195576a9bb6cab1cc66e00dc26790e68e427940
chris-waller <cwaller2@gmail.com>
2020-09-14 18:25:33 -0400

    continuing the refactor of webpack and the scripts that will execute the projects

--- worldwidewaller_applications/server/webpack.config.js
+++ worldwidewaller_applications/server/webpack.config.js
@@ -2,6 +2,7 @@ const webpackMerge = require('webpack-merge');
 const buildValidations = require('../build-utils/build-validations');
 const commonConfig = require('../build-utils/webpack.common');
 
+console.log('in the webpack here');
 // We can include Webpack plugins, through addons, that do
 // not need to run every time we are developing.
 // We will see an example when we set up 'Bundle Analyzer'
@@ -17,20 +18,20 @@ const addons = (/* string | string[] */ addonsArg) => {
 // 'env' will contain the environment variable from 'scripts'
 // section in 'package.json'.
 // console.log(env); => { env: 'dev' }
-module.exports = (env) => {
+module.exports = (env, type) => {
   if (!env) {
     throw new Error(buildValidations.ERR_NO_ENV_FLAG);
   }
-  
-  if (!env.projectType) {
+  if (!type) {
     throw new Error(buildValidations.ERR_NO_TYPE_FLAG);
   }
+  
 
   // Select which Webpack configuration to use; development
   // or production
   // console.log(env.env); => dev
   // eslint-disable-next-line import/no-dynamic-require, global-require
-  const envConfig = require(`../build-utils/${env.projectType}/webpack.${env.env}.js`);
+  const envConfig = require(`../build-utils/webpack.${env.env}.js`);
 
   // 'webpack-merge' will combine our shared configurations, the
   // environment specific configurations and any addons we are
