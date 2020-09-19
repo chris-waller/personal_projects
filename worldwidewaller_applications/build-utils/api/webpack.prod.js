38914b543fb517d4fa7fed21e57fff98e55faceb
chris-waller <cwaller2@gmail.com>
2020-09-19 14:13:44 -0400

    not as stable as previous

--- worldwidewaller_applications/build-utils/api/webpack.prod.js
+++ worldwidewaller_applications/build-utils/api/webpack.prod.js
@@ -1,4 +1,4 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 const commonPaths = require('../common-paths');
 
 console.log('Loading api webpack prod...');
@@ -6,44 +6,11 @@ console.log('Loading api webpack prod...');
 const config = {
   mode: 'production',
   entry: {
-    app: [`${commonPaths.apiAppEntry}/index.js`],
+    server: path.join(commonPaths.apiAppEntry, '/bin/server.js')
   },
   output: {
-    filename: 'static/[name].[hash].js',
+    filename: '[name].js',
   },
   devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.[cs][ac]?ss$/i,
-        use: [
-          {
-            // We configure 'MiniCssExtractPlugin'
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localsConvention: 'camelCase',
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles/styles.[hash].css',
-    }),
-  ],
 };
 module.exports = config;
